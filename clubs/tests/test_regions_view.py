57deeca1a7204ed70ffc9d06a8ab8933527a0773
sdob <sdob@cs.ucc.ie>
2017-01-09 15:15:15 +0000

    Add unit tests for region creation

--- clubs/tests/test_regions_view.py
+++ clubs/tests/test_regions_view.py
@@ -6,6 +6,48 @@ from rest_framework.test import APITestCase
 from clubs.models import Club, Region
 from users.models import User
 
+class RegionViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.north = Region.objects.create(name='north')
+        self.south = Region.objects.create(name='south')
+        self.ucc = Club.objects.create(name='UCC', region=self.south)
+        self.member = User.objects.create_user('Club', 'Member', club=self.ucc)
+
+    def test_admin_can_view_regions(self):
+        self.client.force_authenticate(User.objects.create_superuser('Super', 'User', 'pass'))
+        response = self.client.get(reverse('region-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        expected_length = 2
+        self.assertEqual(len(response.data), expected_length)
+
+    def test_user_can_view_regions(self):
+        self.client.force_authenticate(self.member)
+        response = self.client.get(reverse('region-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        expected_length = 2
+        self.assertEqual(len(response.data), expected_length)
+
+
+class RegionCreateTestCase(APITestCase):
+
+    def setUp(self):
+        self.north = Region.objects.create(name='north')
+        self.south = Region.objects.create(name='south')
+        self.ucc = Club.objects.create(name='UCC', region=self.south)
+        self.member = User.objects.create_user('Club', 'Member', club=self.ucc)
+
+    def test_user_cannot_create_region(self):
+        self.client.force_authenticate(self.member)
+        response = self.client.post(reverse('region-list'), {'name': 'East'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_admin_can_create_region(self):
+        self.client.force_authenticate(User.objects.create_superuser('Super', 'User', 'pass'))
+        response = self.client.post(reverse('region-list'), {'name': 'East'})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
 class RegionDeleteTestCase(APITestCase):
 
     def setUp(self):
