dd030ce3729696b11c46e3a869433fdb34afeab5
sdob <sdob@cs.ucc.ie>
2017-01-09 13:28:01 +0000

    Add custom IsAdmin with object permission check

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -7,6 +7,11 @@ from users.models import User
 def is_admin(user):
     return user.is_staff or user.is_superuser
 
+class IsAdmin(permissions.IsAdminUser):
+
+    def has_object_permission(self, request, view, obj):
+        return request.user and is_admin(request.user)
+
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
