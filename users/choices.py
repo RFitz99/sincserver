dcaf8b0bd505e41f0f771c38c4c9e316ccea8dda
sdob <sdob@cs.ucc.ie>
2017-01-11 10:13:38 +0000

    Document choices.py

--- users/choices.py
+++ users/choices.py
@@ -1,10 +1,20 @@
 from django.utils.translation import ugettext as _
 
+# Some of the attributes of the User object are defined to be one of
+# a fixed set of choices. We don't expect the number of different
+# possible titles to change very often, for example. Internally, we're
+# storing these fields as integers. This file defines the possible
+# fixed choices that each attribute can take. They're tuples of tuples,
+# where the second member of each tuple is a human-readable name for the
+# value.
+
+# Genders. Currently binary.
 GENDER_CHOICES = (
     (0, _('Female')),
     (1, _('Male')),
 )
 
+# Titles. This is the same set of titles used in COMS.
 TITLE_CHOICES = (
     (0, _('Dr')),
     (1, _('Miss')),
@@ -13,6 +23,8 @@ TITLE_CHOICES = (
     (4, _('Ms')),
 )
 
+# Membership types. Members can be 'full' or 'student' divers; this
+# determines the annual membership fee that they pay.
 MEMBERSHIP_FULL = 0
 MEMBERSHIP_STUDENT = 1
 
@@ -21,5 +33,9 @@ MEMBERSHIP_CHOICES = (
     (MEMBERSHIP_FULL, _('Full Diver')),
 )
 
+# Current membership status; this one is a string. We don't actually
+# store this in the database; instead, we compute a user's membership
+# status based on the criteria that determine it (medical assessment,
+# fitness to dive, etc.) and represent it using this value.
 STATUS_CURRENT = 'Current'
 STATUS_LAPSED = 'Lapsed'
