848e5db259ef63ed8b3c1966a026023f4c7d66d1
sdob <sdob@cs.ucc.ie>
2017-01-12 12:36:22 +0000

    Clean up club creation tests

--- clubs/tests/test_club_create.py
+++ clubs/tests/test_club_create.py
@@ -10,44 +10,51 @@ class ClubCreateTestCase(APITestCase):
 
     def setUp(self):
         self.staff = User.objects.create_user(first_name='Staff', last_name='Member', is_staff=True)
-
-    def test_staff_can_create_club(self):
+        # Create a club and a DO
         region = Region.objects.create(name='South')
-        self.client.force_authenticate(self.staff)
-        data = {
+        ucc = Club.objects.create(name='UCC', region=region)
+        self.do = User.objects.create_user('Dave', 'Officer', club=ucc)
+        self.do.become_dive_officer()
+        self.do.save()
+        # Create an RDO
+        self.rdo = User.objects.create_user('Dave', 'Officer', club=ucc)
+        self.rdo.save()
+        # Create a normal user
+        self.user = User.objects.create_user('Normal', 'User')
+        # Create some new club data
+        self.data = {
             'name': 'Daunt',
             'region': region.id,
         }
-        response = self.client.post(reverse('club-list'), data)
+
+    ###########################################################################
+    # Administrators can create clubs.
+    ###########################################################################
+
+    def test_admin_can_create_club(self):
+        self.client.force_authenticate(self.staff)
+        response = self.client.post(reverse('club-list'), self.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    ###########################################################################
+    # Nobody else can create clubs:
+    ###########################################################################
+
     def test_unauthenticated_user_cannot_create_club(self):
-        data = {
-            'name': 'Daunt',
-        }
-        response = self.client.post(reverse('club-list'), data)
+        response = self.client.post(reverse('club-list'), self.data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_user_cannot_create_club(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.post(reverse('club-list'), self.data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
     def test_do_cannot_create_club(self):
-        ucc = Club.objects.create(name='UCC')
-        do = User.objects.create_user('Dave', 'Officer', club=ucc)
-        do.become_dive_officer()
-        do.save()
-        self.client.force_authenticate(do)
-        data = {
-            'name': 'Daunt',
-        }
-        response = self.client.post(reverse('club-list'), data)
+        self.client.force_authenticate(self.do)
+        response = self.client.post(reverse('club-list'), self.data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_rdo_cannot_create_club(self):
-        region = Region.objects.create(name='South')
-        ucc = Club.objects.create(name='UCC', region=region)
-        rdo = User.objects.create_user('Dave', 'Officer', club=ucc)
-        rdo.save()
-        self.client.force_authenticate(rdo)
-        data = {
-            'name': 'Daunt',
-        }
-        response = self.client.post(reverse('club-list'), data)
+        self.client.force_authenticate(self.rdo)
+        response = self.client.post(reverse('club-list'), self.data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
