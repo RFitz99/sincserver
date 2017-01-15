d1822353bee337bab86e537fea2a03485a41f03d
sdob <sdob@cs.ucc.ie>
2017-01-15 14:44:15 +0000

    Add viewset for course enrolments

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,17 @@
 from rest_framework import viewsets
 
-from courses.models import Certificate, Course
-from courses.serializers import CertificateSerializer, CourseSerializer
+from courses.models import Certificate, Course, CourseEnrolment
+from courses.serializers import CertificateSerializer, CourseSerializer, CourseEnrolmentSerializer
 
 class CourseViewSet(viewsets.ModelViewSet):
 
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+class CourseEnrolmentViewSet(viewsets.ModelViewSet):
+
+    queryset = CourseEnrolment.objects.all()
+    serializer_class = CourseEnrolmentSerializer
 
 
 class CertificateViewSet(viewsets.ModelViewSet):
