55b6ee765e63a475321a00299621770451be8b6f
sdob <sdob@cs.ucc.ie>
2017-01-15 18:36:41 +0000

    Add stub CourseViewSet.perform_create

--- courses/views.py
+++ courses/views.py
@@ -8,11 +8,26 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def perform_create(self, serializer):
+        creator = self.request.user
+        organizer = self.request.user
+        club = self.request.user.club
+        region = self.request.user.club.region
+        # Allow staff members to set these fields explicitly
+        if self.request.user.is_staff and ('organizer' in self.request.data):
+            # TODO: implement this
+            pass
+        instance = serializer.save(
+            creator=creator,
+            #club=club,
+            organizer=organizer,
+            region=region,
+        )
+
 class CourseEnrolmentViewSet(viewsets.ModelViewSet):
 
     queryset = CourseEnrolment.objects.all()
     serializer_class = CourseEnrolmentSerializer
-
 
 class CertificateViewSet(viewsets.ModelViewSet):
 
