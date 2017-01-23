b3acc57990dd6aeac81ab12303af9e98f9a72257
sdob <sdob@cs.ucc.ie>
2017-01-23 18:09:16 +0000

    Filter qualification queryset

--- qualifications/views.py
+++ qualifications/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 
+from permissions.permissions import IsAdminUser
 from qualifications.models import Qualification
 from qualifications.serializers import QualificationSerializer
 
@@ -10,6 +11,16 @@ class QualificationViewSet(viewsets.ViewSet):
     queryset = Qualification.objects.all()
 
     def list(self, request, user_pk=None):
-        qualifications = self.queryset.filter(user=user_pk).order_by('-date_granted')
+        qualifications = self.queryset
+        if user_pk is not None:
+            qualifications = self.queryset.filter(user=user_pk)
+
+        user = request.user
+        if not user.is_staff:
+            if user.is_dive_officer():
+                qualifications = qualifications.filter(user__club=user.club)
+            else:
+                qualifications = qualifications.filter(user=user)
+        qualifications = qualifications.order_by('-date_granted')
         serializer = QualificationSerializer(qualifications, many=True)
         return Response(serializer.data)
