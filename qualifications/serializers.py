733a05c746a0b53a5121d74f90c7d02be9ab937c
sdob <sdob@cs.ucc.ie>
2017-01-24 12:37:37 +0000

    Add flat write, nested read

--- qualifications/serializers.py
+++ qualifications/serializers.py
@@ -14,3 +14,8 @@ class QualificationSerializer(serializers.ModelSerializer):
         fields = ('id', 'user', 'certificate', 'date_granted',)
     user = UserSerializer(fields=['id', 'first_name', 'last_name', 'club',])
     certificate = CertificateSerializer()
+
+class QualificationWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Qualification
+        fields = ('id', 'user', 'certificate', 'date_granted',)
--- qualifications/views.py
+++ qualifications/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_condition import C
+from rest_framework import status, viewsets
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 from rest_framework.response import Response
 
-from permissions.permissions import IsAdminUser
-from qualifications.models import Qualification
-from qualifications.serializers import QualificationSerializer
+from permissions.permissions import IsAdminUser, IsSafeMethod
+from users.models import User
+from .models import Certificate, Qualification
+from .serializers import QualificationSerializer, QualificationWriteSerializer
 
-class QualificationViewSet(viewsets.ViewSet):
+class QualificationViewSet(viewsets.ModelViewSet):
 
+    # Users must be authenticated, and only admins can make changes
+    permission_classes = [C(IsAuthenticated) & (C(IsAdminUser) | C(IsSafeMethod))]
     queryset = Qualification.objects.all()
+    serializer_class = QualificationSerializer
+
+    # We want clients to be able to send flat data, e.g.:
+    # {'user': 1, 'certificate': 10}
+    # while receiving nested data, so we'll use two different serializers.
+    def get_serializer(self, *args, **kwargs):
+        kwargs['context'] = self.get_serializer_context()
+        if self.action in SAFE_METHODS:
+            return QualificationSerializer(*args, **kwargs)
+        return QualificationWriteSerializer(*args, **kwargs)
+
+    # Filter the queryset based on the user. Admins can see everything;
+    # DOs can see qualifications issued to members of their own club;
+    # other users can only see qualifications they've received.
+    def get_queryset(self):
+        queryset = Qualification.objects.all()
+        user = self.request.user
+        if user.is_staff:
+            return queryset
+        if user.is_dive_officer():
+            return queryset.filter(user__club=user.club)
+        return queryset.filter(user=user)
 
     def list(self, request, user_pk=None):
         qualifications = self.queryset
