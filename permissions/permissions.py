57ad86b13c1103f50ee1c464226bb2926355de20
sdob <sdob@cs.ucc.ie>
2017-01-24 13:28:27 +0000

    Add new IsUser permission

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -39,6 +39,10 @@ class IsSameUser(permissions.BasePermission):
         return request.user == obj
 
 
+class IsUser(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.user
+
 
 class IsRegionalDiveOfficer(permissions.BasePermission):
     def has_permission(self, request, view):
