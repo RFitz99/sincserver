a4b9148a75657ac859dd22aa897f0420b8edee1d
sdob <sdob@cs.ucc.ie>
2017-01-24 12:46:34 +0000

    Add further tests

--- qualifications/tests/test_qualification_update.py
+++ qualifications/tests/test_qualification_update.py
@@ -25,9 +25,86 @@ class QualificationPatchTestCase(APITestCase):
         self.other_user.receive_certificate(cert)
 
     def test_admin_can_update_qualification_date(self):
-      qual = Qualification.objects.get(user=self.member)
-      date = fake.date_time_this_century().date().strftime('%Y-%m-%d')
-      data = {'date_granted': date}
-      self.client.force_authenticate(self.staff)
-      response = self.client.patch(reverse('qualification-detail', args=[qual.id]), data)
-      self.assertEqual(response.status_code, status.HTTP_200_OK)
+        qual = Qualification.objects.get(user=self.member)
+        date = fake.date_time_this_century().date().strftime('%Y-%m-%d')
+        data = {'date_granted': date}
+        self.client.force_authenticate(self.staff)
+        response = self.client.patch(reverse('qualification-detail', args=[qual.id]), data)
+        # Check HTTP status
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check response data
+        self.assertEqual(response.data['date_granted'], date)
+        # Check database
+        self.assertEqual(
+            Qualification.objects.get(user=self.member).date_granted.strftime('%Y-%m-%d'),
+            date
+        )
+
+    def test_admin_can_update_qualification_certificate(self):
+        qual = Qualification.objects.get(user=self.member)
+        cert = Certificate.objects.create(name='Club Diver')
+        data = {'certificate': cert.id}
+        self.client.force_authenticate(self.staff)
+        response = self.client.patch(reverse('qualification-detail', args=[qual.id]), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check response data
+        self.assertEqual(response.data['certificate'], cert.id)
+        # Check database
+        self.assertEqual(
+            Qualification.objects.get(user=self.member).certificate,
+            cert
+        )
+
+    def test_admin_can_update_qualification_user(self):
+        qual = Qualification.objects.get(user=self.member)
+        data = {'user': self.other_user.id}
+        self.client.force_authenticate(self.staff)
+        response = self.client.patch(reverse('qualification-detail', args=[qual.id]), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check response data
+        self.assertEqual(response.data['user'], self.other_user.id)
+        # Check database
+        self.assertFalse(
+            Qualification.objects.filter(user=self.member).exists()
+        )
+
+    def test_unauthenticated_user_cannot_update_qualification_date(self):
+        qual = Qualification.objects.get(user=self.member)
+        original_date = qual.date_granted
+        date = fake.date_time_this_century().date().strftime('%Y-%m-%d')
+        data = {'date_granted': date}
+        response = self.client.patch(reverse('qualification-detail', args=[qual.id]), data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        # Check database
+        self.assertEqual(
+            Qualification.objects.get(user=self.member).date_granted,
+            original_date
+        )
+
+    def test_user_cannot_update_qualification_date(self):
+        qual = Qualification.objects.get(user=self.member)
+        original_date = qual.date_granted
+        date = fake.date_time_this_century().date().strftime('%Y-%m-%d')
+        data = {'date_granted': date}
+        self.client.force_authenticate(self.member)
+        response = self.client.patch(reverse('qualification-detail', args=[qual.id]), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        # Check database
+        self.assertEqual(
+            Qualification.objects.get(user=self.member).date_granted,
+            original_date
+        )
+
+    def test_dive_officer_cannot_update_qualification_date(self):
+        qual = Qualification.objects.get(user=self.member)
+        original_date = qual.date_granted
+        date = fake.date_time_this_century().date().strftime('%Y-%m-%d')
+        data = {'date_granted': date}
+        self.client.force_authenticate(self.do)
+        response = self.client.patch(reverse('qualification-detail', args=[qual.id]), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        # Check database
+        self.assertEqual(
+            Qualification.objects.get(user=self.member).date_granted,
+            original_date
+        )
